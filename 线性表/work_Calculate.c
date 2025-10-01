33432c361f9611c7656c0f7d5f9d7fc5ea6d8ba7
cheer-sea <1723314903@qq.com>
2025-10-01 22:21:23 +0800

    线性表练习

--- 线性表/work_Calculate.c
+++ 线性表/work_Calculate.c
@@ -19,7 +19,27 @@ void createList(struct Listnode *L, float e, float v) {
     findPosition(L, s);
 }
 
+void findPosition(struct Listnode *L, struct Listnode *s) {
+    struct Listnode *pL, *pre;
+    pre = L;
+    pL = L->next;
 
+    while (1) {
+        if (pL == NULL || pL->exp > s->exp) {
+            break;
+        }
+        
+        if(pL->exp == s->exp) {
+            s->value += pL->value;
+            break;
+        }
+        pre = pL;
+        pL = pL->next;
+    }
+
+    s->next = pL;
+    pre->next = s;
+}
 
 
 
