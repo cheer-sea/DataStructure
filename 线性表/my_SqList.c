0a7b16c01eab834d6935f3888fec44f82e3082ec
cheer-sea <1723314903@qq.com>
2025-10-05 23:37:52 +0800

    线性表练习

---
+++ 线性表/my_SqList.c
@@ -0,0 +1,47 @@
+// 这个源文件，尝试线性表的顺序存储表示和相关操作
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXSIZE 100
+#define Status int
+#define OK 1
+#define ERROR -1
+#define OVERFLOW -2
+
+typedef struct {
+    int money;
+    char id[20];
+} ElemType;
+
+typedef struct {
+    ElemType *elem;
+    int length;
+} SqList;
+
+int main() {
+    SqList L;
+}
+
+Status InitList(SqList *L) {
+// 初始化顺序表
+    L->elem = (ElemType *) malloc(MAXSIZE * sizeof(ElemType));
+    if (L->elem == NULL) {
+        return OVERFLOW;
+    }
+    L->length = 0;
+    return OK;
+}
+
+Status GetElem(SqList L, int i, ElemType *e) {
+// 获取顺序表第i个元素
+    if (i < 1 || i > L.length) {
+        return ERROR;
+    }
+
+    e = L.elem[i - 1];
+    return OK;
+}
+
+
+
