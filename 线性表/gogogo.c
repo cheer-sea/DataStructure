b04d505f2cf0bfba9ecb583e382c1f181d0f0558
cheer-sea <1723314903@qq.com>
2025-10-22 23:02:12 +0800

    线性表练习

--- 线性表/gogogo.c
+++ 线性表/gogogo.c
@@ -1,1 +1,16 @@
-while ()
+#include <stdio.h>
+
+void change(int a[]);
+int main(){
+    int a[10];
+    a[0] = 2;
+    change(a);
+    printf("%d", a[0]);
+
+    return 0;
+}
+
+void change(int a[]) {
+    a[0] = 5;
+    return;
+}
