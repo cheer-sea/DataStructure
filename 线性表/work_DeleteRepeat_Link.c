b92c4563be29b17a417bcfc38f70e6ae9e4e35a3
cheer-sea <1723314903@qq.com>
2025-10-29 23:40:04 +0800

    线性表练习

--- 线性表/work_DeleteRepeat_Link.c
+++ 线性表/work_DeleteRepeat_Link.c
@@ -1,44 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define MAXSIZE 105
 
 typedef struct {
     int address;
     int value;
-    int follow;
+    int next;
 } ElemType;
 
-typedef struct Node {
-    ElemType data;
-    struct Node *next;
-} LNode, *LinkList;
-
-void InitList(LinkList L) {
-    L->next = NULL;
-}
-void CreateList(LinkList L, int n) {
-    for (int i = 0; i < n; i++) {
-        int address, value, follow;
-        scanf("%d%d%d", &address, &value, &follow);
-        ElemType e = {address, value, follow};
-        LNode *p = (LNode *)malloc(sizeof(LNode));
-        p->data = e;
-        p->next = L->next;
-        L->next = p;
-    }
-}
-
-void MergeList(LinkList L1, LinkList Res_L, int n, int finish[n]) {
-    LNode *p = L1->next;
-    int cnt = 0;
-    while (cnt < n) {
-        
-    }
-    
-
-
-}
+typedef struct  {
+    ElemType data[MAXSIZE];
+    int length;
+} SqList;
 
 int main() {
+    SqList L;
+    int start, n;
+    
+    scanf("%d%d", &start, &n);
+    for (int i = 0; i < n; i++) {
+        int address, value, next, abs_value;
+        scanf("%5d%d%5d", &address, &value, &next);
+
+        L.data[i].address = address;
+        L.data[i].value = value;
+        L.data[i].next = next;
+    }
+
+    int front, new_start = start, count = 0, num = 0;
+    int flag[100000] = {0}, finish[105] = {0};
+    while (count < n) {
+        for (int i = 0; i < n; i++) {
+            if (finish[i]) 
+                continue;
+            if (L.data[i].address == new_start) {
+                int abs_value = abs(L.data[i].value);
+
+                num++;
+                new_start = L.data[i].next;
+                finish[i] = 1;
+                count++;
+
+                if (flag[abs_value]) {
+                    num--;
+                    L.data[i].address = -2;
+                    L.data[front].next = new_start;
+                }
+                flag[abs_value] = 1;
+                front = i;
+            }
+        }
+    }
+    printf("%d\n", num);
+
+    int re_finish[105] = {0};
+    new_start = start, count = 0;
+    while (count < num) {
+        for (int i = 0; i < n; i++) {
+            if (L.data[i].address == new_start) {
+                if (L.data[i].next == -1) {
+                    printf("%05d %d %d\n", L.data[i].address, L.data[i].value, L.data[i].next);
+                    break;
+                }
+                printf("%05d %d %05d\n", L.data[i].address, L.data[i].value, L.data[i].next);
+                new_start = L.data[i].next;
+            }
+        }
+        count++;
+    }
+    
     return 0;
 }
 
