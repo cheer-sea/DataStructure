832b38d3543835ef41746e004acc36d3f7660f2e
cheer-sea <1723314903@qq.com>
2025-11-28 23:57:17 +0800

    树练习

--- 树与二叉树/myUPSet.c
+++ 树与二叉树/myUPSet.c
@@ -21,3 +21,28 @@ void Union(int s[], int root1, int root2) {
     if (root1 == root2) return;
     s[root2] = root1;
 }
+
+
+int Find_opti(int s[], int x) {
+// 查操作的优化，“压缩路径”提高速度
+    int root = x;
+    while (s[root] > 0) root = s[root];
+    while (x != root) {
+        int t = s[x];
+        s[x] = root;
+        x = t;
+    }
+    return root;
+}
+
+void Union_opti(int s[], int root1, int root2) {
+    if (root1 == root2) return;
+    if (s[root2] > s[root1]) {
+        s[root1] += s[root2];
+        s[root2] = root1;
+    }
+    else {
+        s[root2] += s[root1];
+        s[root1] = root2;
+    }
+}
