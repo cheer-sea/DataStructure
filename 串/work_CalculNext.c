6a94e5304614f8ef067c49a4db9ca2685cd12433
cheer-sea <1723314903@qq.com>
2025-11-30 16:26:31 +0800

    串练习

--- 串/work_CalculNext.c
+++ 串/work_CalculNext.c
@@ -1,24 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 int main() {
     int n, next[10000] = {0};
     char str[10000];
 
     scanf("%d", &n);
-    scanf("%s", str);
+    char c = getchar();
+    for (int i = 0; i < n; i++) {
+        scanf("%c", &str[i]);
+        c = getchar();
+    }
 
     next[0] = -1;
-    for (int i = 1; i < n; i++) {
-        if (str[i] == str[next[i-1]]) {
-            next[i] = next[i-1] + 1;
+    int i = 0, j = -1;
+    int len = strlen(str);
+
+    while (i < n-1) {
+        if (j == -1 || str[i] == str[j]) {
+            next[++i] = ++j;
         }
         else {
-            int j = i-1;
-            while (j != 0 || str[next[j]] != str[i]) {
-                j = next[j];
-            }
-            next[i] = j;
+            j = next[j];
         }
     }
 
