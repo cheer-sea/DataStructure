bd66e94032e394e9004f0654bcc3e2dc5b17800c
cheer-sea <1723314903@qq.com>
2025-12-24 23:10:03 +0800

    排序练习

---
+++ 排序/my_InsrtSort.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void InsertSort(int A[], int n) {
+    int i, j, temp;
+    for (int i = 1; i < n; i++) {
+        if (A[i] < A[i-1]) {
+            temp = A[i];
+            for (j = i-1; j >= 0 && A[j] > temp; j--) {
+                A[j+1] = A[j];
+            }
+            A[j+1] = temp;
+        }
+    }
+}
